acf2435647519a795be3f80bc778efabc6982bb6
itsyosefali <joeyxjoey123@gmail.com>
2023-11-01 10:52:46 +0200

    if not dmc then pass

--- multi_items_select/events/stock_entry.py
+++ multi_items_select/events/stock_entry.py
@@ -13,11 +13,11 @@ def on_update(doc, method):
                     "item_code": item.item_code,
                 },
             )
-            
+
             if len(item_stock_data) > 0:
                 data = item_stock_data[0]
                 transferable_qty = data.actual_qty - data.reserved_qty
-                if int(item.qty) > transferable_qty:
+                if int(item.qty) > transferable_qty and not doc.masm_device_maintenance_card:
                     frappe.msgprint(
                         f"Item <string>{item.item_code}</strong> Qty ({item.qty}) is higher than the Transferable Qty ({transferable_qty}) at row {item.idx}",
                         "Message",
